1c97f99b3066528a179a92b2cec3032031d68e3d
= <=>
2026-07-11 14:37:27 +0700

    edit search

--- search/src/main/java/com/yas/search/config/ElasticsearchDebugConfig.java
+++ search/src/main/java/com/yas/search/config/ElasticsearchDebugConfig.java
@@ -1,46 +1,48 @@
-// package com.yas.search.config;
+package com.yas.search.config;
 
-// import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
-// import org.apache.hc.core5.http.EntityDetails;
-// import org.apache.hc.core5.http.HttpRequest;
-// import org.apache.hc.core5.http.protocol.HttpContext;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.elasticsearch.client.ClientConfiguration;
-// import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-// import org.springframework.data.elasticsearch.client.elc.rest5_client.Rest5Clients;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
+import org.apache.hc.core5.http.EntityDetails;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.rest5_client.Rest5Clients;
 
-// @Configuration
-// public class ElasticsearchDebugConfig extends ElasticsearchConfiguration {
+@Configuration
+@Profile("!test")
+public class ElasticsearchDebugConfig extends ElasticsearchConfiguration {
 
-//     private static final Logger log = LoggerFactory.getLogger(ElasticsearchDebugConfig.class);
+    private static final Logger log = LoggerFactory.getLogger(ElasticsearchDebugConfig.class);
 
-//     @Override
-//     public ClientConfiguration clientConfiguration() {
-//         String u = System.getenv("ELASTICSEARCH_USERNAME") != null ? System.getenv("ELASTICSEARCH_USERNAME") : "yas";
-//         String p = System.getenv("ELASTICSEARCH_PASSWORD") != null ? System.getenv("ELASTICSEARCH_PASSWORD") : "LarUmB3A49NTg9YmgW4=";
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        String u = System.getenv("ELASTICSEARCH_USERNAME") != null ? System.getenv("ELASTICSEARCH_USERNAME") : "yas";
+        String p = System.getenv("ELASTICSEARCH_PASSWORD") != null ? System.getenv("ELASTICSEARCH_PASSWORD") : "LarUmB3A49NTg9YmgW4=";
 
-//         return ClientConfiguration.builder()
-//                 .connectedTo("elasticsearch-es-http.elasticsearch:9200")
-//                 .withBasicAuth(u, p)
-//                 .withClientConfigurer(
-//                     Rest5Clients.ElasticsearchHttpClientConfigurationCallback.from(
-//                         (HttpAsyncClientBuilder httpClientBuilder) -> {
-//                             httpClientBuilder.addRequestInterceptorFirst((HttpRequest request, EntityDetails entity, HttpContext context) -> {
+        return ClientConfiguration.builder()
+                .connectedTo("elasticsearch-es-http.elasticsearch:9200")
+                .withBasicAuth(u, p)
+                .withClientConfigurer(
+                    Rest5Clients.ElasticsearchHttpClientConfigurationCallback.from(
+                        (HttpAsyncClientBuilder httpClientBuilder) -> {
+                            httpClientBuilder.addRequestInterceptorFirst((HttpRequest request, EntityDetails entity, HttpContext context) -> {
 
-//                                 log.info(">>>>>>> PRINTING OUTBOUND HTTP REQUEST TO ES >>>>>>>");
-//                                 log.info("Request Line: {} {}", request.getMethod(), request.getRequestUri());
+                                log.info(">>>>>>> PRINTING OUTBOUND HTTP REQUEST TO ES >>>>>>>");
+                                log.info("Request Line: {} {}", request.getMethod(), request.getRequestUri());
 
-//                                 java.util.Arrays.stream(request.getHeaders()).forEach(header ->
-//                                     log.info("Header -> {}: {}", header.getName(), header.getValue())
-//                                 );
-//                                 log.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-//                             });
-//                             return httpClientBuilder;
-//                         }
-//                     )
-//                 )
-//                 .build();
-//     }
-// }
+                                java.util.Arrays.stream(request.getHeaders()).forEach(header ->
+                                    log.info("Header -> {}: {}", header.getName(), header.getValue())
+                                );
+                                log.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                            });
+                            return httpClientBuilder;
+                        }
+                    )
+                )
+                .build();
+    }
+}
